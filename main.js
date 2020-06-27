e7e0e06f4660a1e7b7f824ac51b345b43621343a
Amy Hawthorne <amy.hawthorne@gmail.com>
2020-06-27 16:15:06 -0700

    added main.js

--- main.js
+++ main.js
@@ -1,16 +1,94 @@
-$(document).ready(() => {
-    $('#js-shopping-list-form').submit(function (event) {
-        $('.li').append($('#shopping-list-entry'));
-        event.preventDefault();
-        });
-    },
-    $('.shopping-item-toggle').toggleClass( shopping-item__checked, addOrRemove));
+$(function() {
 
-    $('.shopping-item-delete').on('click', () => {
-       $(event.target).closest($('.shopping-item')).hide(); 
+    $('#js-shopping-list-form').on('submit',(function (event) {
+    event.preventDefault(); 
+    var newItem = $('#shopping-list-entry').val(); 
+    $('.shopping-list').append(`<li>
+    <span class="shopping-item">${newItem}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle">
+        <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button>
+    </div>
+    </li>`);
+    }));
+    
+
+    
+  $('ul').on('click', '.shopping-item-toggle', function (event) {  
+      $(this).closest($('li')).find($('.shopping-item')).toggleClass('shopping-item__checked');
     });
 
-  
 
+  $('ul').on('click', '.shopping-item-delete', function (event) {
+      $(this).closest($('li')).hide(); 
+    });
+  
+  });
+
+
+
+
+
+
+
+
+
+/*
+A FEW OF MANY FALSE STARTS
+/*
+function showEnteredItems() {
+    var x = ($('#shopping-list-entry')).value;
+    document.getElementById($('shopping-item')).innerHTML = x;
+  }
+showEnteredItems(); 
+*/ 
+
+/*
+$('.shopping-item-toggle').on('click', () => {
+    $(event.target).closest($('.shopping-item').toggleClass('shopping-item__checked'));
+});
+*/
+
+/*{
+    $('#js-shopping-list-form').submit( () => {
+            var txt1 = $("#shopping-list-entry")
+        $('.shopping-list').append($('.li', 'txt1');
+        event.preventDefault();
+    }
+    });
+
+
+  $('.shopping-item-toggle').on('click', () => {
+        $(event.target).closest($('li')).find($('.shopping-item')).toggleClass('shopping-item__checked');
+        event.stopPropagation();
+        console.log('1234');
+    });
+
+
+  if ('newItem' === 0) {
+  $('#js-shopping-list-form').off('submit'),
+  };
+
+      ($('.shopping-item')).change,() => { 
+    $('.shopping-item-toggle').on('click', () => {
+        $(event.target).closest($('li')).find($('.shopping-item')).toggleClass('shopping-item__checked');
+    });
+
+    $('.shopping-item-delete').on('click', () => {
+       $(event.target).closest($('li')).hide(); 
+    });
+  };
+
+
+      $('.shopping-item-controls').on('click', 'shopping-item-toggle', () => {
+      //event.preventDefault();
+      $(event.target).closest($('li')).find($('.shopping-item')).toggleClass('shopping-item__checked');
+  });
+
+*/
 
 
